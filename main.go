e7869e0f3a23d94f777664a368e75eeb1e6a4dd4
VitorD <vitordaltro8@gmail.com>
2025-09-13 13:04:47 -0300

    Implements CRUD methods using MongoDB

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type creditCard struct {
@@ -12,54 +18,123 @@ type creditCard struct {
 	UserId     string `json:"userId"`
 }
 
-var creditCards = []creditCard{
-	{ID: "1", CardNumber: "1234656727", UserId: "123"},
+func getAllVirtualCreditCards(collection *mongo.Collection) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		filter := bson.D{}
+
+		cursor, _ := collection.Find(context.TODO(), filter)
+		var creditCards []creditCard
+
+		if err := cursor.All(context.TODO(), &creditCards); err != nil {
+			panic(err)
+		}
+
+		c.IndentedJSON(http.StatusOK, creditCards)
+	}
 }
 
-func getAllVirtualCreditCards(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, creditCards)
+func getVirtualCreditCardById(collection *mongo.Collection) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		filter := bson.D{{Key: "id", Value: id}}
+
+		result := collection.FindOne(context.TODO(), filter)
+		var creditCard creditCard
+
+		if err := result.Decode(&creditCard); err != nil {
+			panic(err)
+		}
+
+		c.IndentedJSON(http.StatusOK, creditCard)
+	}
 }
 
-func getVirtualCreditCardById(c *gin.Context) {
-	id := c.Param("id")
+func updateVirtualCreditCard(collection *mongo.Collection) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
 
-	for _, a := range creditCards {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
+		var creditCard creditCard
+		if err := c.BindJSON(&creditCard); err != nil {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+		}
+
+		filter := bson.D{{Key: "id", Value: id}}
+
+		update := bson.D{{Key: "$set", Value: bson.D{{Key: "cardnumber", Value: creditCard.CardNumber}}}}
+
+		result := collection.FindOneAndUpdate(context.TODO(), filter, update)
+
+		c.IndentedJSON(http.StatusOK, result)
+	}
+}
+
+func createVirtualCreditCard(collection *mongo.Collection) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		var newCreditCard creditCard
+
+		if err := c.BindJSON(&newCreditCard); err != nil {
 			return
 		}
+
+		_, err := collection.InsertOne(context.TODO(), newCreditCard)
+		if err != nil {
+			log.Fatal("Could not save the virtual credit card id: " + newCreditCard.ID)
+			return
+		}
+
+		c.IndentedJSON(http.StatusNoContent, nil)
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"messsage": "credit card not found"})
+
 }
 
-func creatVirtualCreditCard(c *gin.Context) {
-	var newCreditCard creditCard
+func deleteVirtualCreditCard(collection *mongo.Collection) gin.HandlerFunc {
 
-	if err := c.BindJSON(&newCreditCard); err != nil {
-		return
+	return func(c *gin.Context) {
+		id := c.Param("id")
+
+		filter := bson.D{{Key: "id", Value: id}}
+
+		_, err := collection.DeleteOne(context.TODO(), filter)
+		if err != nil {
+			panic(err)
+		}
+
+		c.IndentedJSON(http.StatusNoContent, nil)
 	}
-	creditCards = append(creditCards, newCreditCard)
-
-	c.IndentedJSON(http.StatusNoContent, nil)
-}
-
-func deleteVirtualCreditCard(c *gin.Context) {
-	var newCreditCard creditCard
-
-	if err := c.BindJSON(&newCreditCard); err != nil {
-		return
-	}
-	creditCards = append(creditCards, newCreditCard)
-
-	c.IndentedJSON(http.StatusNoContent, nil)
 }
 
 func main() {
+	uri := os.Getenv("MONGODB_URI")
+	docs := "www.mongodb.com/docs/drivers/go/current/"
+
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable" +
+			"See: " + docs +
+			"usage-examples/#environment-variable")
+	}
+
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("db").Collection("creditCards")
+
 	router := gin.Default()
-	router.GET("/creditCards", getAllVirtualCreditCards)
-	router.GET("/creditCards/:id", getVirtualCreditCardById)
-	router.POST("/creditCards", creatVirtualCreditCard)
-	router.DELETE("/creditCards", deleteVirtualCreditCard)
+	router.GET("/creditCards", getAllVirtualCreditCards(coll))
+	router.GET("/creditCards/:id", getVirtualCreditCardById(coll))
+	router.POST("/creditCards", createVirtualCreditCard(coll))
+	router.DELETE("/creditCards/:id", deleteVirtualCreditCard(coll))
+	router.PUT("/creditCards/:id", updateVirtualCreditCard(coll))
 
 	router.Run("localhost:5000")
 }
