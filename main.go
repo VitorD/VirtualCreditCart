f7c7b4ae4efe927da457b50dcd4945b360cd89a7
VitorD <vitordaltro8@gmail.com>
2025-09-13 10:34:13 -0300

    Create Http methods with simple example

--- main.go
+++ main.go
@@ -16,13 +16,50 @@ var creditCards = []creditCard{
 	{ID: "1", CardNumber: "1234656727", UserId: "123"},
 }
 
-func getCreditCards(c *gin.Context) {
+func getAllVirtualCreditCards(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, creditCards)
+}
+
+func getVirtualCreditCardById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range creditCards {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"messsage": "credit card not found"})
+}
+
+func creatVirtualCreditCard(c *gin.Context) {
+	var newCreditCard creditCard
+
+	if err := c.BindJSON(&newCreditCard); err != nil {
+		return
+	}
+	creditCards = append(creditCards, newCreditCard)
+
+	c.IndentedJSON(http.StatusNoContent, nil)
+}
+
+func deleteVirtualCreditCard(c *gin.Context) {
+	var newCreditCard creditCard
+
+	if err := c.BindJSON(&newCreditCard); err != nil {
+		return
+	}
+	creditCards = append(creditCards, newCreditCard)
+
+	c.IndentedJSON(http.StatusNoContent, nil)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/creditCards", getCreditCards)
+	router.GET("/creditCards", getAllVirtualCreditCards)
+	router.GET("/creditCards/:id", getVirtualCreditCardById)
+	router.POST("/creditCards", creatVirtualCreditCard)
+	router.DELETE("/creditCards", deleteVirtualCreditCard)
 
 	router.Run("localhost:5000")
 }
