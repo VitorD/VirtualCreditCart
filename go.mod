474861e5e8387c742ad4505e192b9b1199b1f180
VitorD <vitordaltro8@gmail.com>
2025-09-15 09:40:00 -0300

    Migrate to PostgreSQL using docker as database

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/gin-gonic/gin v1.10.1
 	go.mongodb.org/mongo-driver v1.17.4
 	go.mongodb.org/mongo-driver/v2 v2.3.0
+	gorm.io/driver/postgres v1.6.0
+	gorm.io/gorm v1.31.0
 )
 
 require (
@@ -20,6 +22,12 @@ require (
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
--- main.go
+++ main.go
@@ -1,140 +1,109 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/v2/bson"
-	"go.mongodb.org/mongo-driver/v2/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-type creditCard struct {
-	ID         string `json:"id"`
+// Modelo da tabela
+type CreditCard struct {
+	ID         uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	CardNumber string `json:"cardNumber"`
 	UserId     string `json:"userId"`
 }
 
-func getAllVirtualCreditCards(collection *mongo.Collection) gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		filter := bson.D{}
-
-		cursor, _ := collection.Find(context.TODO(), filter)
-		var creditCards []creditCard
-
-		if err := cursor.All(context.TODO(), &creditCards); err != nil {
-			panic(err)
-		}
-
-		c.IndentedJSON(http.StatusOK, creditCards)
-	}
-}
-
-func getVirtualCreditCardById(collection *mongo.Collection) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		id := c.Param("id")
-
-		filter := bson.D{{Key: "id", Value: id}}
-
-		result := collection.FindOne(context.TODO(), filter)
-		var creditCard creditCard
-
-		if err := result.Decode(&creditCard); err != nil {
-			panic(err)
-		}
-
-		c.IndentedJSON(http.StatusOK, creditCard)
-	}
-}
-
-func updateVirtualCreditCard(collection *mongo.Collection) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		id := c.Param("id")
-
-		var creditCard creditCard
-		if err := c.BindJSON(&creditCard); err != nil {
-			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
-		}
-
-		filter := bson.D{{Key: "id", Value: id}}
-
-		update := bson.D{{Key: "$set", Value: bson.D{{Key: "cardnumber", Value: creditCard.CardNumber}}}}
-
-		result := collection.FindOneAndUpdate(context.TODO(), filter, update)
-
-		c.IndentedJSON(http.StatusOK, result)
-	}
-}
-
-func createVirtualCreditCard(collection *mongo.Collection) gin.HandlerFunc {
-
-	return func(c *gin.Context) {
-		var newCreditCard creditCard
-
-		if err := c.BindJSON(&newCreditCard); err != nil {
-			return
-		}
-
-		_, err := collection.InsertOne(context.TODO(), newCreditCard)
-		if err != nil {
-			log.Fatal("Could not save the virtual credit card id: " + newCreditCard.ID)
-			return
-		}
-
-		c.IndentedJSON(http.StatusNoContent, nil)
+// Inicializa conexão com o PostgreSQL
+func initDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=postgres dbname=creditCard port=5423 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Erro ao conectar no banco: ", err)
 	}
 
-}
-
-func deleteVirtualCreditCard(collection *mongo.Collection) gin.HandlerFunc {
-
-	return func(c *gin.Context) {
-		id := c.Param("id")
-
-		filter := bson.D{{Key: "id", Value: id}}
-
-		_, err := collection.DeleteOne(context.TODO(), filter)
-		if err != nil {
-			panic(err)
-		}
-
-		c.IndentedJSON(http.StatusNoContent, nil)
+	// Cria a tabela se não existir
+	err = db.AutoMigrate(&CreditCard{})
+	if err != nil {
+		log.Fatal("Erro ao migrar schema: ", err)
 	}
+
+	return db
 }
 
 func main() {
-	uri := os.Getenv("MONGODB_URI")
-	docs := "www.mongodb.com/docs/drivers/go/current/"
-
-	if uri == "" {
-		log.Fatal("Set your 'MONGODB_URI' environment variable" +
-			"See: " + docs +
-			"usage-examples/#environment-variable")
-	}
-
-	client, err := mongo.Connect(options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-
-	coll := client.Database("db").Collection("creditCards")
-
+	db := initDB()
 	router := gin.Default()
-	router.GET("/creditCards", getAllVirtualCreditCards(coll))
-	router.GET("/creditCards/:id", getVirtualCreditCardById(coll))
-	router.POST("/creditCards", createVirtualCreditCard(coll))
-	router.DELETE("/creditCards/:id", deleteVirtualCreditCard(coll))
-	router.PUT("/creditCards/:id", updateVirtualCreditCard(coll))
+
+	// Listar todos os cartões
+	router.GET("/creditCards", func(c *gin.Context) {
+		var cards []CreditCard
+		if err := db.Find(&cards).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, cards)
+	})
+
+	// Buscar cartão por ID
+	router.GET("/creditCards/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var card CreditCard
+		if err := db.First(&card, id).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Cartão não encontrado"})
+			return
+		}
+		c.JSON(http.StatusOK, card)
+	})
+
+	// Criar cartão
+	router.POST("/creditCards", func(c *gin.Context) {
+		var newCard CreditCard
+		if err := c.BindJSON(&newCard); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "JSON inválido"})
+			return
+		}
+		if err := db.Create(&newCard).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusCreated, newCard)
+	})
+
+	// Atualizar cartão
+	router.PUT("/creditCards/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var card CreditCard
+		if err := db.First(&card, id).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Cartão não encontrado"})
+			return
+		}
+
+		var updateData CreditCard
+		if err := c.BindJSON(&updateData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "JSON inválido"})
+			return
+		}
+
+		// Atualiza apenas os campos enviados
+		if err := db.Model(&card).Updates(updateData).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, card)
+	})
+
+	// Deletar cartão
+	router.DELETE("/creditCards/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		if err := db.Delete(&CreditCard{}, id).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.Status(http.StatusNoContent)
+	})
 
 	router.Run("localhost:5000")
 }
